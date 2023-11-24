3781ce9a2bfd5f32f78b106bb523a28dfe063eb7
Beirão <phsicopt@gmail.com>
2023-11-24 17:04:08 +0000

    Something

---
+++ Files/push_swap.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseferr <joseferr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 16:24:22 by joseferr          #+#    #+#             */
+/*   Updated: 2023/11/24 16:24:22 by joseferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa()
+{
+	ft_prinft("sa\n");
+}
+
+void	sb()
+{
+	ft_prinft("sb\n");
+}
+
+void	ss()
+{
+	ft_prinft("ss\n");
+}
+
+void	pa()
+{
+	ft_prinft("pa\n");
+}
+
+void	pb()
+{
+	ft_prinft("pb\n");
+}
+
+void	rb()
+{
+	ft_prinft("rb\n");
+}
+
+void	rr()
+{
+	ft_prinft("rr\n");
+}
+
+void	rra()
+{
+	ft_prinft("rra\n");
+}
+
+void	rrr()
+{
+	ft_prinft("rrr\n");
+}
+
+void a_is_sorted()
+{
+
+}
+
+void sort_small_stack()
+{
+
+}
+
+void sort_big_stack()
+{
+
+}
+
+
+int	main(int argc, char** argv)
+{
+	int	error;
+	t_list	**stack_a;
+	t_list	**stack_b;
+
+	error = 0;
+	if (argc < 2)
+		return (-1);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	stack_b = (t_list **)malloc(sizeof(t_list));
+	*stack_a = NULL;
+	*stack_b = NULL;
+	while (argc - 1)
+	{
+		if(a == (int)a)
+		{
+
+		}
+		argc --;
+	}
+
+
+	if(error = 1)
+	{
+		ft_prinft("Error\n");
+	}
+	return(0);
+}
---
+++ Files/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseferr <joseferr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 16:58:25 by joseferr          #+#    #+#             */
+/*   Updated: 2023/11/24 16:58:25 by joseferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include
+
+
+#endif
